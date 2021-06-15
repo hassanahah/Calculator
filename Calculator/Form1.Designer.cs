5d8fbd1a91633eff1175150650165eb203b780f2
Hossein Ebrahimi <57136190+h-Ebr@users.noreply.github.com>
2021-06-15 14:19:28 +0430

    Added tab control.

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Calculator
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,61 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Tabs = new System.Windows.Forms.TabControl();
+            this.ClassicTab = new System.Windows.Forms.TabPage();
+            this.AdvancedTab = new System.Windows.Forms.TabPage();
+            this.Tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tabs
+            // 
+            this.Tabs.Controls.Add(this.ClassicTab);
+            this.Tabs.Controls.Add(this.AdvancedTab);
+            this.Tabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Tabs.Location = new System.Drawing.Point(0, 0);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.SelectedIndex = 0;
+            this.Tabs.Size = new System.Drawing.Size(334, 461);
+            this.Tabs.TabIndex = 0;
+            // 
+            // ClassicTab
+            // 
+            this.ClassicTab.Location = new System.Drawing.Point(4, 22);
+            this.ClassicTab.Name = "ClassicTab";
+            this.ClassicTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ClassicTab.Size = new System.Drawing.Size(326, 435);
+            this.ClassicTab.TabIndex = 0;
+            this.ClassicTab.Text = "Classic";
+            this.ClassicTab.UseVisualStyleBackColor = true;
+            // 
+            // AdvancedTab
+            // 
+            this.AdvancedTab.Location = new System.Drawing.Point(4, 22);
+            this.AdvancedTab.Name = "AdvancedTab";
+            this.AdvancedTab.Padding = new System.Windows.Forms.Padding(3);
+            this.AdvancedTab.Size = new System.Drawing.Size(326, 435);
+            this.AdvancedTab.TabIndex = 1;
+            this.AdvancedTab.Text = "Advanced";
+            this.AdvancedTab.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.Tabs);
+            this.Name = "FrmMain";
+            this.Text = "Calculator";
+            this.Tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.TabPage ClassicTab;
+        private System.Windows.Forms.TabPage AdvancedTab;
     }
 }
 
